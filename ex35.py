d9e1ffaadc98e7827fcdc66743d38d7af59865c3
Yan Hui <leilayanhui@sina.com>
2017-04-06 16:51:30 +0800

    bear_room() done

--- ex35.py
+++ ex35.py
@@ -16,8 +16,30 @@ def gold_room():
         dead('You greedy bastard!')
 
 
+def bear_room():
+    print ('There is a bear here.')
+    print ('The bear has a bunch of honey.')
+    print ('The fat bear is in front of another door.')
+    print ('How are you going to move the bear?')
+    bear_moved = False
+
+    while True:
+        choice = input("> ")
+
+        if choice == 'take honey':
+            dead("The bear looks at you then slaps your face off.")
+        elif choice == 'taunt bear' and not bear_moved:
+            print ('The bear has moved from the door. You can go through if now.')
+            bear_moved = True
+        elif choice == 'taunt bear' and bear_moved:
+            dead('The bear gets pissed off and chews your leg off.')
+        elif choice == 'open door' and bear_moved:
+            gold_room()
+        else:
+            print ('I got no idea what that means.')
+
 def dead(why):
     print (why, 'Good job!')
     exit(0)
 
-gold_room()
+bear_room()
